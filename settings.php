816657c2fd0dc8fa2630d44c8d0039d3c07cc3c0
Vinh Hoang <vinh.donhoang@gmail.com>
2025-03-19 21:36:17 +0700

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Settings</title>
+</head>
+<body>
+    <?php 
+       $host = "feenix-mariadb-web.swin.edu.au"; // Database host
+       $username = "s105545302"; // Your database username
+       $password = "140506"; // Your database password
+       $database = "s105545302_db"; // Your database name       
+    ?>
+</body>
+</html>
