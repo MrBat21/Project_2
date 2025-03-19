33370eb7134375b66568e131916b94429a519473
Vinh Hoang <vinh.donhoang@gmail.com>
2025-03-19 21:35:53 +0700

    Update apply.php

--- apply.php
+++ apply.php
@@ -6,40 +6,10 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
-    <body>
-        <header>
-        <a href="index.php"><img loading="lazy" class="logo" src="styles/images/logo sfc (1).png" alt="SFC_Logo"></a>
-        <nav>
-            <div class="menu">
-            <ul>
-                    <li>
-                    <span><i class="fa fa-home" aria-hidden="true"></i></span>   
-                    <span><a href="index.php">Home</a></span>
-                    </li>
-                    <li>
-                    <span><i class="fa fa-sitemap" aria-hidden="true"></i></span>
-                    <span><a href="about_us.php">About</a></span>
-                    </li>
-                    <li>
-                    <span><i class="fa fa-code" aria-hidden="true"></i></span>
-                    <span><a href="jobs.php">Job</a></span>
-                    </li>
-                    <li>
-                    <span><i class="fa fa-cogs" aria-hidden="true"></i></span>
-                    <span><a href="management.php">Management</a></span>
-                    </li>
-                    <li>
-                    <span><i class="fa fa-desktop" aria-hidden="true"></i></span>
-                    <span><a href="apply.php">Apply</a></span>
-                    </li>
-                    <li>
-                    <span><i class="fa fa-edit" aria-hidden="true"></i></span>
-                    <span><a href="enhancements.php">Enhancement</a></span>
-                    </li>
-            </ul>
-            </div>
-        </nav>
-        </header>
+            <?php
+            include('menu.inc')
+            ?>
+        <hr>
         <div class="intro">
             <div class="text-box">
             <h1>Application Form</h1>
