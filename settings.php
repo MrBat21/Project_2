44152829bfbe19aea33bff9fb2fd4e4d283c4042
Vinh Hoang <vinh.donhoang@gmail.com>
2025-03-21 15:13:11 +0700

    Update settings.php

--- settings.php
+++ settings.php
@@ -6,10 +6,12 @@
 </head>
 <body>
     <?php 
-       $host = "feenix-mariadb-web.swin.edu.au"; // Database host
+       $host = "feenix-mariadb.swin.edu.au"; // Database host
        $username = "s105545302"; // Your database username
        $password = "140506"; // Your database password
-       $database = "s105545302_db"; // Your database name       
-    ?>
+       $sql_db = "s105545302_db"; // Your database name       
+
+?>
+
 </body>
 </html>
