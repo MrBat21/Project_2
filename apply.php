2a79d876c65c5439b8b2cfc0fff5bba21fbb4ce1
Vinh Hoang <vinh.donhoang@gmail.com>
2025-03-26 22:59:18 +0700

    Update apply.php

--- apply.php
+++ apply.php
@@ -103,33 +103,8 @@
                 </div>
             </form>
         </div>
-        <div class="grid">
-            <div class="contact-text">
-                <h3 class="White-text">We always listen to you</h3>
-                <h2 class="Gold-text">Contact us</h2>
-            </div>
-            <div class="contact-pic">
-                <a class="facebook" href="https://www.facebook.com/profile.php?id=100072274716909">
-                <span class="facebook">
-                <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
-                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
-                </svg>
-                </span>
-                Our Facebook page</a>
-                <a class="mail" href="swinburnefootballclub.hcmc@gmail.com">
-                    <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
-                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
-                    </svg>
-                    </span>
-                Our Mail
-                </a>
-            </div>
-        </div>
-
-        <footer>
-        <div class="footer">&#169; 2025 SFC, All rights reserved.</div>
-        <div class="contact">Contact Us: example@gmail.com</div>
-        </footer>
+        <?php
+            include('footer.inc')
+            ?>
 </body>
 </html>
